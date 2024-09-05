5b0a3069821272f888fa5768f3fce1330d9046b3
hotelcurtains <114700124+hotelcurtains@users.noreply.github.com>
2024-09-05 19:09:11 -0400

    9-5 most of lab1

--- lab1/sqrt.cpp
+++ lab1/sqrt.cpp
@@ -6,26 +6,37 @@
  * Description: Computes the square root of a command-line argument.
  * Pledge  : I pledge my honor that I have abided by the Stevens Honor System.
  ******************************************************************************/
+
+/* https://github.com/hspahic-cs/Stevens-IT/blob/3e11c00d56837fa4774b66849e91a63dbb5e9e34/CS385-TA%20-%20Algorithms/Algorithms/sqrt/sqrt.cpp */
+#include <iomanip>
+#include <limits>
 #include <iostream>
 using namespace std;
 
-int main(void)
-{
-    double num;
-    double epsilon = 0.0000001;
-    cin >> num;
-    cin >> epsilon;
 
-    
-    
-}
-
-double sqrt(double num, double epsilon){
-    if (num <= 0)
+double sqrt(double num, double epsilon = 0.0000001){
+    if (num < 0)
         return numeric_limits<double>::quiet_NaN();
     else if (num == 0 || num == 1)
         return num;
-    else
-        return 0;
-    return 0;
+    else {
+        double next_guess = num;
+        double last_guess;
+        while(abs(next_guess - last_guess) > epsilon){
+		    last_guess = next_guess;
+		    next_guess = (next_guess + num / next_guess) * 0.5;
+	    }
+        return next_guess;
+    }
 }
+
+int main(void){
+    double num;
+    double epsilon;
+
+    cin >> num >> epsilon;
+
+    cout << fixed << setprecision(8) << sqrt(num, epsilon) << endl;
+    
+}
+
