29f75d4ed3cd617838e96e6d8c9c387d78ffca6a
hotelcurtains <114700124+hotelcurtains@users.noreply.github.com>
2024-10-24 18:47:41 -0400

    quickselect

--- coinflipsort/coinflipsort.cpp
+++ coinflipsort/coinflipsort.cpp
@@ -17,6 +17,7 @@ static void swap(int array[], const int a, const int b) {
     array[b] = temp;
 }
 
+/*
 static void print_array(int array[], const int length){
     cout << "[";
     for (int i = 0; i < length-1; i++){
@@ -24,6 +25,7 @@ static void print_array(int array[], const int length){
     }
     cout << array[length-1] << "]" << endl;
 }
+*/
 
 static bool is_sorted(int array[], const int length){
     for (int i = 0; i < length-1; i++){
@@ -44,8 +46,34 @@ void coinflipsort(int array[], int length){
             if (r == 1) swap(array, i, i+1);
         }
     }
-    print_array(array, length);
+    //print_array(array, length);
     cout << "Swaps: " << swaps << endl;
+}
+
+void bubble_sort(int array[], const int length) {
+    int swaps = 0;
+    for(int i = 0; i < length - 1; i++) {
+        for(int j = 0; j < length - 1 - i; j++) {
+            if(array[j + 1] < array[j]) {
+               swaps++;
+               swap(array, j, j + 1);
+            }
+        }
+    }
+    //print_array(array, length);
+    cout << "Swaps: " << swaps << endl;
+}
+
+void bogo_sort(int array[], const int length) {
+    int shuffles = 0;
+    while(!is_sorted(array, length)){
+        for (int i = 0; i < length; i++){ // shuffle
+            shuffles++;
+            swap(array, i, rand() % length);
+        }
+    }
+    //print_array(array, length);
+    cout << "Shuffles: " << shuffles << endl;
 }
 
 int main(int argc, char * const argv[]) {
@@ -66,16 +94,34 @@ int main(int argc, char * const argv[]) {
     srand(time(0));
 
     int* array = new int[n];
+    int* array2 = new int[n];
+    int* array3 = new int[n];
     for (int i = 0; i < n; i++){
-        array[i] = (int)rand();
+        int k = (int)rand();
+        array[i] = k;
+        array2[i] = k;
+        array3[i] = k;
     }
-        
-    auto start = high_resolution_clock::now();
 
-    coinflipsort(array,n);
-    
+    cout << "Bubble sort:" << endl;
+    auto start = high_resolution_clock::now();
+    bubble_sort(array2,n);
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Time: " << duration.count() << " microseconds" << endl;
+
+    cout << "Coin-flip sort:" << endl;
+    start = high_resolution_clock::now();
+    coinflipsort(array,n);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+    cout << "Time: " << duration.count() << " microseconds" << endl;
+
+    cout << "Bogosort:" << endl;
+    start = high_resolution_clock::now();
+    bogo_sort(array3,n);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
     cout << "Time: " << duration.count() << " microseconds" << endl;
 
     delete [] array;
